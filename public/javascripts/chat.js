3d9f957e47b90051f7228f2273aef4d526893a66
Yong Wang <yowang@splunk.com>
2017-12-29 16:36:07 +0800

    before off the clock

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -8,4 +8,33 @@ Chat.prototype.sendMessage = function(room, text) {
         text: text
     };
     this.socket.emit('message', message);
-}
+} 
+
+Chat.prototype.processCommand = function(command) {
+    var words = command.split(' ');
+    var command = words[0]
+                    .substring(1, words[0].length)
+                    .toLowerCase();
+    var message = false;
+
+    switch(comnad) {
+        case 'join':
+            words.shift();
+            var room = words.join(' ');
+            this.changeRoom(room);
+            break;
+        
+        case 'nick':
+            words.shift();
+            var name = words.join(' ')
+            this.socket.emit('nameAttempt', name);
+            break;
+        
+        default:
+            message = 'Unrecognized command.';
+            break;
+    }
+
+    return message;
+
+};
